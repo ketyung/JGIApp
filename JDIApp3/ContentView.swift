6b846c174990765f33580dfab3d22c0b3b8e3679
techchee <ketyung@techchee.com>
2021-08-01 20:14:55 +0800

    Initial Commit

---
+++ JDIApp3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JDIApp3
+//
+//  Created by Chee Ket Yung on 01/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
